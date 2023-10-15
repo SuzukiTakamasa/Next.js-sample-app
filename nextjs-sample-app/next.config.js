519f2876917aba50a8fa6573d75fc882ee487eed
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-10-15 23:00:43 +0900

    first commit

---
+++ nextjs-sample-app/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+     reactStrictMode: true,
+     compiler: {
+        styedComponents: true,
+     },
+}
+
+module.exports = nextConfig
